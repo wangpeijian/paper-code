0cd3924d457a911caa5c69815c8a1de06538b503
王佩剑 <519955464@qq.com>
2021-05-04 17:51:08 +0800

    初始化项目

---
+++ src/main/java/com/wpj/paper/PaperApplication.java
@@ -0,0 +1,13 @@
+package com.wpj.paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperApplication.class, args);
+    }
+
+}
